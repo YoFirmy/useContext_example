60c541748d6fa40161c8f41c4a0b68810662f10f
YoFirmy <mattfirmston@gmail.com>
2023-10-11 15:14:53 +0100

    create server component as child of context provider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import CounterButtons from "@/components/CounterButtons";
 import CounterDisplay from "@/components/CounterDisplay";
+import Heading from "@/components/Heading";
 import { CounterProvider } from "@/context/counterContext";
 
 export default function Home() {
   return (
     <main className="p-4 flex flex-col items-center">
-      <h1 className="mb-8">Context Example</h1>
       <CounterProvider>
+        <Heading />
         <CounterDisplay />
         <CounterButtons />
       </CounterProvider>
---
+++ src/components/Heading.tsx
@@ -0,0 +1,4 @@
+export default function Heading() {
+  console.log("server? or client?");
+  return <h1 className="mb-8">Context Example</h1>;
+}
